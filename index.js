831e4ad9c7a53758f2c9875e6f89492d7a4e843b
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-02-13 15:49:23 +0100

    add random beer on click, get id from random beer, try to show content in infopage

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { fetchRandomBeer } from "./randomBeer.js";
-import { addRandomBeerToDom, changePage} from "./utilities.js";
+import { addRandomBeerToDom} from "./utilities.js";
 
-fetchRandomBeer("https://api.punkapi.com/v2/beers/random").then((randomBeers) => {
+
+
+ fetchRandomBeer("https://api.punkapi.com/v2/beers/random").then((randomBeers) => {
   addRandomBeerToDom(randomBeers);
 });
 
-changePage();
+
+
+
--- utilities.js
+++ utilities.js
@@ -1,5 +1,8 @@
 import { pageContent } from "./randomBeer.js";
-const infoPageContent = document.querySelector(".blockTillUsed");
+import { fetchRandomBeer } from "./randomBeer.js";
+import { fetchSingleBeer } from "./infoPage.js";
+const infoPage = document.querySelector(".block");
+const ipageContent = document.getElementById("infoPage");
 export async function addRandomBeerToDom(randomBeer) {
   let getRandomBeerToDom = randomBeer
     .map((randomBeer) => {
@@ -8,7 +11,7 @@ export async function addRandomBeerToDom(randomBeer) {
         imgSrc: `${randomBeer.image_url}`,
         index: `${randomBeer.id}`,
       };
-
+      getSingleBeerWithId(beerCard.index);
       return `
            <nav><a>search</a></nav>
            <section class="cardAndButton">
@@ -25,14 +28,65 @@ export async function addRandomBeerToDom(randomBeer) {
     })
     .join("");
   pageContent.innerHTML = getRandomBeerToDom;
+  const button = document.querySelector(".button");
+  const seeMore = document.querySelector(".seeMore");
+
+  seeMore.addEventListener("click", () => updatePage());
+  button.addEventListener("click", () => fetchOnButtonClick());
 }
-export function changePage() {
-  if (window.location.hash !== null) {
-    infoPageContent.classList.remove("blockTillUsed");
-  }
+
+function fetchOnButtonClick() {
+  fetchRandomBeer("https://api.punkapi.com/v2/beers/random").then((randomBeer) => {
+    addRandomBeerToDom(randomBeer);
+  });
 }
-// export function getIdFromUrl(id) {
-//   let idWithHash = document.location.hash;
-//   console.log(idWithHash);
-//    id = idWithHash.replace("#","");
-// }
+function updatePage() {
+  pageContent.classList.add("block");
+  infoPage.classList.remove("block");
+  infoPage.classList.add("infoPage");
+}
+
+export function getSingleBeerWithId(beerId) {
+  console.log(beerId);
+  fetchSingleBeer(`https://api.punkapi.com/v2/beers/${beerId}`).then((singleBeer) => {
+    addSingleBeerToDom(singleBeer);
+  });
+}
+
+function addSingleBeerToDom(singleBeer) {
+  console.log(singleBeer);
+  let singleBeerToDom = singleBeer
+    .map((beer) => {
+      const beerInfo = {
+        name: `${beer.name}`,
+        imgSrc: `${beer.image_url}`,
+        index: `${beer.id}`,
+        description: `${beer.description}`,
+        abv: `${beer.abv}`,
+        volume: `${beer.volume}`,
+        ingredients: `${beer.ingredients}`,
+        hops: `${beer.hops}`,
+        food_pairing: `${beer.food_pairing}`,
+        brewersTips: `${beer.brewers_tips}`,
+      };
+      return `
+     <section class="info-container"> 
+     <img src="${beerInfo.imgSrc}" class="infoImg" alt="infoImg" />
+     <h1>${beerInfo.name}, information </h1>
+     <p>${beerInfo.description}</p>
+     <h2> Details</h2>
+     <h6>- Abv(alcohol by volume), ${beerInfo.abv}</h6>
+     <h6>- Volume, ${beerInfo.volume}</h6>
+     <h2> Ingredients</h2>
+     <h6> ${beerInfo.ingredients}</h6>
+     <h2> Hops</h2>
+     <h6> ${beerInfo.hops}</h6>
+     <h2> Food pairing</h2>
+     <h6> ${beerInfo.food_pairing}</h6>
+      <h2> Tips</h2>
+     <h6> ${beerInfo.brewersTips}</h6>
+     </section>`;
+    })
+    .join("");
+  ipageContent.innerHtml = singleBeerToDom;
+}
